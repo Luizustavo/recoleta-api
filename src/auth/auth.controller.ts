242d156f4d2c0844465371cc2c19af3e58716ca2
Luizustavo <luizg.ss@outlook.com>
2025-04-30 15:03:58 -0300

    feat(auth): implement sign-in functionality with JWT and bcrypt validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  async signIn(@Body() body: Prisma.UserCreateInput) {
+    return this.authService.signIn(body);
+  }
+}
