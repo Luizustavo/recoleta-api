c81fd4827e20f6ab5997d439e40d72e940b83036
Luizustavo <luizg.ss@outlook.com>
2025-04-30 12:31:46 -0300

    feat(user): implement UserService methods and UserController for user management

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
-
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Prisma, User as UserModel } from '@prisma/client';
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async signupUser(
+    @Body() userData: Prisma.UserCreateInput,
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
+  }
+}
