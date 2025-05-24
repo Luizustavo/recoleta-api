b6822875625edca560669701c09a097ccdbeccac
Luizustavo <luizg.ss@outlook.com>
2025-05-24 03:08:43 -0300

    feat: add Mongoose integration and update dependencies in package.json

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,20 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
 import { AddressModule } from './address/address.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UserModule, DatabaseModule, AddressModule],
+  imports: [
+    MongooseModule.forRoot(String(process.env.DATABASE_URL), {
+      ssl: true,
+      tlsAllowInvalidCertificates: true,
+      retryWrites: true,
+      w: 'majority',
+    }),
+    AuthModule,
+    UserModule,
+    DatabaseModule,
+    AddressModule,
+  ],
 })
 export class AppModule {}
